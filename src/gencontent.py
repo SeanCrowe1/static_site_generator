605f7ca4c5dc96fa395ef573b692b4e601c36304
SeanCrowe1 <sean.crowe2002@gmail.com>
2025-11-09 16:23:04 +0000

    GitHub Pages functionality test

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,26 +1,48 @@
 import os
-import shutil
-
-from copystatic import copy_files_recursive
-from gencontent import generate_pages_recursive
+from pathlib import Path
+from markdown_blocks import markdown_to_html_node
 
 
-dir_path_static = "./static"
-dir_path_public = "./public"
-dir_path_content = "./content"
-template_path = "./template.html"
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
-def main():
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+def generate_page(from_path, template_path, dest_path, basepath):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
 
-    print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
 
-    print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
 
 
-main()
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
+
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -18,25 +25,8 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    for current_dir, subdirs, files in os.walk(dir_path_content):
-        for file in files:
-            if file.endswith(".md"):
-                from_path = os.path.join(current_dir, file)
-                inter_path = from_path.replace("./content", "./public")
-                root, ext = os.path.splitext(inter_path)
-                dest_path = root + ".html"
-                generate_page(
-                    from_path,
-                    template_path,
-                    dest_path
-                )
-
-    print("Generating pages...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
