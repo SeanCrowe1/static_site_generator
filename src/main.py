7cc1749acf38abb3829fa5e5030301d5ba7da4a6
SeanCrowe1 <sean.crowe2002@gmail.com>
2025-11-09 13:29:28 +0000

    text node class implemented

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    new_obj = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(new_obj)
+
+if __name__ == "__main__":
+    main()
